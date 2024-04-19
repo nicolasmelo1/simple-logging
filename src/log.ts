f2282a5292f52f206b8e31593c79b1ed29e27499
Nicolas Melo <nicolasmelo12@gmail.com>
2024-04-19 17:11:20 -0300

    second commit

--- src/log.ts
+++ src/log.ts
@@ -1,5 +1,9 @@
 export async function POST(request: Request) {
   console.log(await request.json());
 
+  return new Response("Received Log!");
+}
+
+export async function GET(request: Request) {
   return new Response("Hello, World!");
 }
