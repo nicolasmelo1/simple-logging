6d6872ea46ba03e684efd0df2854231cacd5c468
Nicolas Melo <nicolasmelo12@gmail.com>
2024-10-10 16:18:10 -0300

    apply change to remove the header from the request

--- src/log.ts
+++ src/log.ts
@@ -1,9 +1,9 @@
 export async function POST(request: Request) {
-  console.log(Array.from(request.headers.entries()), await request.json());
+  console.log(await request.json());
   return new Response("Received Log!");
 }
 
 export async function GET(request: Request) {
-  console.log(Array.from(request.headers.entries()), request);
+  console.log(request);
   return new Response("App is up and running!");
 }
